70abc28360184636e7645228320036b07a69d63f
dvic <info@dvic.io>
2022-09-12 07:58:05 +0200

    Fix tests for new instrumentation_scope

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 import Config
 
 if config_env() == :test do
-  config :opentelemetry, processors: [{:ot_batch_processor, %{scheduled_delay_ms: 1}}]
+  config :opentelemetry, processors: [{:otel_batch_processor, %{scheduled_delay_ms: 1}}]
 end
